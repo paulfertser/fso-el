55e55e8fe5d15287a27def1c1af750a7b0fc9806
Paul Fertser <fercerpav@gmail.com>
2012-11-13 20:34:20 +0400

    Rename function used to message from call log to avoid name clashing
    This should fix regression introduced by the previous commit.
    
    Signed-off-by: Paul Fertser <fercerpav@gmail.com>

--- fso.el
+++ fso.el
@@ -671,7 +671,7 @@ method for answering a call during e.g. driving."
   (fso-gsm-initiate-call
    (cdr (assoc "Peer" (assq (ewoc-data (ewoc-locate buffer-ewoc)) fso-pim-calls)))))
 
-(defun fso-pim-call-selected-message ()
+(defun fso-pim-message-selected-call ()
   (interactive)
   (fso-gsm-send-message nil
 			(cdr (assoc "Peer" (assq (ewoc-data (ewoc-locate buffer-ewoc)) fso-pim-calls)))
@@ -691,7 +691,7 @@ method for answering a call during e.g. driving."
     (let ((keymap (copy-keymap (current-local-map))))
       (define-key keymap "r" 'fso-pim-call-selected-call)
       (define-key keymap "d" 'fso-pim-delete-selected-call)
-      (define-key keymap "M" 'fso-pim-call-selected-message)
+      (define-key keymap "M" 'fso-pim-message-selected-call)
       (use-local-map keymap))
     (setq buffer-read-only t)
     (buffer-disable-undo)
